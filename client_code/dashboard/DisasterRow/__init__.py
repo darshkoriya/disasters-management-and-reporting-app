21c90faab915e0423744a0f5f1424afe8ea8395e
kumardarsh60@gmail.com <kumardarsh60@gmail.com>
2025-12-09 15:09:31 +0000

    Edited Login, Start, dashboard, dashboard.BloodDonateRow and 11 more

--- client_code/dashboard/DisasterRow/__init__.py
+++ client_code/dashboard/DisasterRow/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import DisasterRowTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
 import anvil.server
 # Define colors based on severity
 severity_colors = {
--- client_code/dashboard/EditUser/__init__.py
+++ client_code/dashboard/EditUser/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import EditUserTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
 import anvil.server
 import anvil.tables.query as q
 import base64
